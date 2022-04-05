657ba8605213b740d3fa4b46049fa45142f1e077
António Neves <aluno23321@ipt.pt>
2022-04-05 17:25:02 +0100

    Migration e adição do controller veterinários + opção para efetuar um file upload

--- Vets/Vets/Models/Donos.cs
+++ Vets/Vets/Models/Donos.cs
@@ -2,12 +2,12 @@
 
 namespace Vets.Models {
     public class Donos {
-        
+
         /// <summary>
         /// Representa os dados do Dono de um animal
         /// </summary>
         public Donos() {
-            ListaAnimais = new HashSet<Animais>();    
+            ListaAnimais = new HashSet<Animais>();
         }
 
         /// <summary>
@@ -19,20 +19,31 @@ namespace Vets.Models {
         /// Nome do dono do aninal
         /// </summary>
         [Required(ErrorMessage = "O {0} é preenchimento obrigatório")]
+        [StringLength(20, ErrorMessage = "O {0} não pode ter mais do que {1} caracteres")]
+        [RegularExpression("[A-ZÂÓÍa-záéíóúàèìòùâêîôûãõäëïöüñç '-]+", ErrorMessage = "No{0} só são aceites letras")]
         public string Nome { get; set; }
 
         /// <summary>
         /// NIF do dono do animal
         /// </summary>
         [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+        [StringLength(9, MinimumLength = 9 , ErrorMessage = "O {0} deve ter exatamente {1} caracteres ")]
+        [RegularExpression("[123578]+[0-9]{8}", ErrorMessage = "O {0} deve começar por 1, 2, 3, 5, 7 ou 8 e só ter algarimos")]
         public string NIF { get; set; }
-        
+
         /// <summary>
         /// sexo do dono
         /// Ff - feminino / Mm - masculino
         /// </summary>
-
+        [StringLength(1, ErrorMessage = "O {0} tem que ter exatamente {1] caracter")]
+        [RegularExpression("[FfMm]", ErrorMessage = "No {0} só se aceitam as letras F ou M")]
         public string Sexo { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        [EmailAddress(ErrorMessage = "Introduza um email correto, por favor")]
+        public string Email { get; set; }
 
         /// <summary>
         /// lista dos animais de quem o Dono é dono
--- Vets/Vets/Models/Veterinarios.cs
+++ Vets/Vets/Models/Veterinarios.cs
@@ -1,17 +1,30 @@
 ﻿namespace Vets.Models {
     public class Veterinarios {
-
+        /// <summary>
+        /// modelo que interage com os dados
+        /// </summary>
         public Veterinarios() {
             ListaConsultas = new HashSet<Consultas>();
         }
+        /// <summary>
+        /// ID do veterinário
+        /// </summary>
         public int Id { get; set; }
-
+        /// <summary>
+        /// Nome do veterinário
+        /// </summary>
         public string Nome { get; set; }
-
+        /// <summary>
+        /// Nº da cedula profissional
+        /// </summary>
         public string NumCedulaProf { get; set; }
-
+        /// <summary>
+        /// Nomo do ficheiro que contem a fotografia do Veterinario
+        /// </summary>
         public string Fotografia { get; set; }
-
+        /// <summary>
+        /// Lista das consultas
+        /// </summary>
         public ICollection<Consultas> ListaConsultas { get; set; }
 
     }
